70e11a1a84b86bd3abcf14b68782a8a2402f149f
samangan <harry.hull1@gmail.com>
2013-10-14 14:47:47 -0500

    almost done implementing Brighten()

--- shadejs/src/shades.js
+++ shadejs/src/shades.js
@@ -1,41 +1,80 @@
 
 (function () {
+	var elements = {};
+
+	//TODO: make the following configurable
+	//var brightenFactor = ;
+	//var darkenFactor = ;
 	
 	var Shadesjs = window.Shadesjs = function Shadesjs() {
-		//TODO: Implement
-
+		elements = Shadesjs._getAllElementsToShade();
 	};	
 
 	Shadesjs._getAllElementsToShade = function( ) {
 		var elementToShade = Shadesjs.getConfig("elementToShade");
 		if(elementToShade === true) {
-			// TODO: return all elements on the page
+			return document.getElementsByTagName("*");
 		} 
 		if(elementToShade === false) {
 			return;
 		}
-		//TODO: return only the elementToShade selector
+		return document.getElementsByTagName(elementToShade); //TODO: does this work as intended?
 	};	
 
 	Shadesjs._getColor = function(element) {
-		console.log(element);
 		var color = window.getComputedStyle(element, null).getPropertyValue("background-color");
-		console.log(color);
 		return color;
 	};
 
-	/* TODO:
-		* function that increases brightness for elementToShade elements
-		* function that decreases brightness for elementToShade elements
-	*/
+	Shadesjs._setColor = function(element, color) {
+		element.style.backgroundColor = color;
+	};
 
-	// Below is configuration functionality:
+	Shadesjs._getRGBA = function(color) {
+		if (typeof color === 'string' || color instanceof String){
+			return color.match(/\d+/g);
+		} else {
+			console.log("Error: Trying to get the RGBA of a non string.");
+		}
+	};
+
+	Shadesjs.Brighten = function ( ) {
+		//TODO: Implement
+		if (elements) {
+			for (var i = 0; i < elements.length; i++) {
+				var color = Shadesjs._getColor(elements[i]);
+
+				if (color) {
+					var rgbaVals = Shadesjs._getRGBA(color);
+					
+					var r = rgbaVals[0];
+					var g = rgbaVals[1];
+					var b = rgbaVals[2];
+					if (rgbaVals.length === 4){
+						var a = rgbaVals[3];
+						Shadesjs._setColor(elements[i], "rgba(" + r + ", " + g + ", " + b + ", " + a + ")");
+					} else {
+						Shadesjs._setColor(elements[i], "rgb(" + r + ", " + g + ", " + b + ")");
+					}
+				}
+			}
+		}
+	};
+
+	Shadesjs.Darken = function ( ) {
+		//TODO: Implement
+	};
+
+	/* TODO:
+		*  ('public') function that increases brightness for elementToShade elements
+		*  ('public') function that decreases brightness for elementToShade elements
+	*/
 
 	Shadesjs._configuration = {};
 	Shadesjs._defaultConfiguration = {
 		// Set to a jquery selector to only shade certain elements
-		// Set to true to shade all elements on the page
-		// Set to false to shade no elements on the page
+		// If set to true to shade all elements on the page
+		// If set to false to shade no elements on the page
 		elementToShade: true
 	};
 
@@ -55,7 +94,10 @@
 		//TODO: implement
 	};
 
-
-
+	// Load Shadesjs
+	window.onload = onLoad;
+	function onLoad() {
+		Shadesjs();
+	}
 
 })();
